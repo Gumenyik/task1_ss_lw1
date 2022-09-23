def67c403591a88833a1e33839dc355b3a60e766
Taras Humeniuk <taras181716@gmail.com>
2022-09-23 23:20:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.IO;
+namespace task1
+{
+     static class Program
+    {
+        static void Main()
+        {
+            string url = "https://mail.univ.net.ua/readme.txt";
+            string savePath = @"D:\Save\readme.txt";
+            WebClient client = new WebClient();
+            client.DownloadFile(url, savePath);
+            Console.ReadLine();
+        }
+    }
+}
