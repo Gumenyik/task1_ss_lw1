c4cbee3defc9ac2749479dd058bf215f6268dd13
Gumenyik <78902763+Gumenyik@users.noreply.github.com>
2022-09-24 23:03:54 +0300

    Little differrent with version 1

--- Program.cs
+++ Program.cs
@@ -5,12 +5,18 @@ namespace task1
 {
      static class Program
     {
+        void Write()
+        {
+            string savePathLigh = @"D:\Save\ReadMe-LIGHT.txt";
+            using (StreamReader sr = new StreamReader(savePath));
+        } 
         static void Main()
         {
             string url = "https://mail.univ.net.ua/readme.txt";
             string savePath = @"D:\Save\readme.txt";
             WebClient client = new WebClient();
             client.DownloadFile(url, savePath);
+            Console.WriteLine(savePath);
             Console.ReadLine();
         }
     }
